7c100ddf2213af6dfc45e7b23a503c2fdf1dfc4b
Garen Torikian <gjtorikian@gmail.com>
2012-12-03 12:40:26 -0800

    Just some UX updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-A website.
+A website highlighting some of the ways GitHub could increase its ROI of Awesomeness
+by 328%.
+
+It was created by [this guy](https://github.com/gjtorikian).
--- js/ux.js
+++ js/ux.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
+	var isResume = /resume/.test(window.location.pathname);
+
 	$('.toc').scrollspy();
 	$('#navbar').scrollspy();
 
 	$('.toc-sidenav').affix({
 	  offset: {
 	    top: function () { return $(window).width() <= 980 ? 290 : 210 },
-	    bottom: 270
+	    bottom: 70
 	  }
 	});
 
 	$(".scroll").click(function(event){		
 	    event.preventDefault();
-	    $('html,body').animate({scrollTop:$(this.hash).offset().top - 25}, 500);
+	    $('html,body').animate({scrollTop:$(this.hash).offset().top - (isResume ? 25 : 0) }, 500);
 	});
 });
